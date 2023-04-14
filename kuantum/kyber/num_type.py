3488cb746b27eecef30c9215061588437c5b6d73
Rakshit Arora <rakshit087@gmail.com>
2023-04-14 18:38:30 +0530

    added NumType class defination

---
+++ kuantum/kyber/num_type.py
@@ -0,0 +1,17 @@
+class NumType:
+
+    def byte(self, x):
+        pass
+
+    def int16(self, x):
+        pass
+
+    def uint16(self, x):
+        pass
+
+    def int32(self, x):
+        pass
+    
+    def uint32(self, x):
+        pass
+
