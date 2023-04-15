c6dda2088bd93fb711926e5f28f7b2f5d228f103
Rakshit Arora <rakshit087@gmail.com>
2023-04-15 17:20:23 +0530

    moved functional definations to utils

---
+++ kuantum/kyber/utils/poly.py
@@ -0,0 +1,49 @@
+# adds two polynomials.
+def poly_add(a, b):
+    pass
+
+# subtracts two polynomials.
+def poly_sub():
+    pass
+
+def poly_barret_reduce():
+    pass
+
+def poly_montgomery_reduce():
+    pass
+
+def poly_base_mul():
+    pass
+
+def poly_to_bytes(a):
+    '''
+    Serialize a polynomial to a byte array.
+
+    arg0: polynomial
+    '''
+    pass
+
+def poly_from_bytes(a):
+    '''
+    Deserialize a polynomial from a byte array.
+
+    arg0: byte array
+    '''
+    pass
+
+def poly_from_msg(a):
+    '''
+    Convert a 32-byte message to a polynomial.
+
+    arg0: 32-byte message
+    '''
+    pass
+
+def poly_to_msg(a):
+    '''
+    Convert a polynomial to a 32-byte message.
+
+    arg0: polynomial
+    '''
+    pass
+
