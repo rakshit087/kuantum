c0703a6f6fe0185a802d3970a1f4b9b11771c9e0
Rakshit Arora <rakshit087@gmail.com>
2023-04-15 14:35:12 +0530

    added byte ops

--- kuantum/kyber/byte_ops.py
+++ kuantum/kyber/byte_ops.py
@@ -1,33 +1,62 @@
-import numpy as np
+from num_type import int16, int32, uint16, uint32
+from constants import PARAMS_Q_INV, PARAMS_Q, PARAMS_N, PARAMS_ETA_1, PARAMS_ETA_2
 
-class ByteOps:
 
-    # returns a 24-bit unsigned integer loaded from byte x.
-    def byte_to_24_bit_uint(self, byte):
-        r = np.uint32(byte[0])
-        r |= (np.uint32(byte[1]) << 8)
-        r |= (np.uint32(byte[2]) << 16)
-        return r
+def load24_bit(byte):
+    '''
+    load 3 bytes into a 32-bit integer needed for Kyber 512
+    arg0: input byte array
+    return:  32-bit unsigned integer 
+    '''
+    r = int32(byte[0])
+    r |= (int32(byte[1]) << 8)
+    r |= (int32(byte[2]) << 16)
+    return r
+
+
+def load32_bit(byte):
+    '''
+    load 4 bytes into a 32-bit integer needed for Kyber 768 
+    and Kyber 1024
     
-    # returns a 32-bit unsigned integer loaded from byte x.
-    def byte_to_32_bit_uint(self, byte):
-        r = np.uint32(byte[0])
-        r = r | (np.uint32(byte[1]) << 8)
-        r = r | (np.uint32(byte[2]) << 16)
-        r = r | (np.uint32(byte[3]) << 24)
-        return r
+    arg0: input byte array
+    return:  32-bit unsigned integer
+    '''
+    r = uint32(byte[0])
+    r = r | (uint32(byte[1]) << 8)
+    r = r | (uint32(byte[2]) << 16)
+    r = r | (uint32(byte[3]) << 24)
+    return r
+
+
+def gen_cbd_pol(buff, eta):
+    '''
+    compute polynomial with coefficients distributed according to
+    a centered binomial distribution 
+
+    arg0: array of uniformly random bytes
+    arg1: eta = 2 for Kyber 512 and eta = 3 Kyber 768 / Kyber 1024
     
-    # computes a polynomial with coefficients distributed
-    # according to a centered binomial distribution with parameter eta,
-    # given an array of uniformly random bytes.
-    def gen_cbd_pol(self, buff, eta):
-        pass
-
-    # computes a Barrett reduction given a 16 bit integer
-    def barrett_reduce(self, a):
-        pass
-
-    # computes a Montgomery reduction given a 32 bit integer
-    # returns `a * R^-1 mod Q` where `R=2^16`
-    def montgomery_reduce(self, a):
-        pass
+    return: array with coefficients
+    '''
+    r = [0] * 384
+    if eta == 2:
+        for i in range(0, PARAMS_N // 4):
+            t = load24_bit(buff[3 * i:])
+            d = t & 0x00249249
+            d += ((t >> 1) & 0x00249249)
+            d += ((t >> 2) & 0x00249249)
+            for j in range(0, 4):
+                a = int16((d >> (6 * j + 0)) & 0x7)
+                b = int16((d >> (6 * j + PARAMS_ETA_1)) & 0x7)
+                r[4 * i + j] = (a - b)
+    else:
+        for i in range(0, PARAMS_N // 8):
+            t = load32_bit(buff[4 * i:])
+            d = t & 0x55555555
+            d = d + ((t >> 1) & 0x55555555)
+            for j in range(0, 8):
+                a = int16((d >> (4 * j + 0)) & 0x3)
+                b = int16((d >> (4 * j + PARAMS_ETA_2)) & 0x3)
+                r[8 * i + j] = (a - b)
+    return r
