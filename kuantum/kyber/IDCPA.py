32046ecfa2a44b5fc5902780a4cb7093808bf4ac
Rakshit Arora <rakshit087@gmail.com>
2023-04-15 17:19:40 +0530

    idcpa and kyber class definations

---
+++ kuantum/kyber/IDCPA.py
@@ -0,0 +1,13 @@
+class IDCPA:
+
+    def gen_matrix(self):
+        pass
+
+    def idcpa_keypair(self):
+        pass
+
+    def idcpa_enc(self):
+        pass
+
+    def idcpa_dec(self):
+        pass
---
+++ kuantum/kyber/Kyber.py
@@ -0,0 +1,13 @@
+class Kyber():
+
+    def __init__(self, type):
+        self.type = type
+    
+    def gen_keypair(self):
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
