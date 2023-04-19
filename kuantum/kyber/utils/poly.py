e868f511a53f28cbaabe33767d0b9bab6b674b18
Rakshit Arora <rakshit087@gmail.com>
2023-04-20 04:11:47 +0530

    finished remaining poly functions

--- kuantum/kyber/utils/poly.py
+++ kuantum/kyber/utils/poly.py
@@ -1,5 +1,10 @@
-from constants import PARAMS_N, PARAMS_Q, COMPRESSED_BYTES_512, COMPRESSED_BYTES_1024
-from num_type import int16, int32, uint16, uint32, byte
+from constants import PARAMS_N, PARAMS_Q, NTT_ZETAS, PARAMS_ETA_1, PARAMS_ETA_2
+from constants import COMPRESSED_BYTES_512, COMPRESSED_BYTES_1024, POLY_BYTES
+from num_type import int16, uint16, uint32, byte
+from reduce import barrett_reduce, montgomery_reduce
+from ntt import base_multiplier
+from byte_ops import gen_cbd_pol
+from Crypto.Hash import SHAKE256
 from types import List
 
 def poly_add(a, b):
@@ -33,17 +38,50 @@ def poly_conditional_sub_q(r: List[int]):
         r[i] += (r[i] >> 31) & int16(PARAMS_Q)
     return r
 
-def poly_barret_reduce():
-    pass
+def poly_barret_reduce(r):
+    """
+    Applies Barrett reduction to all coefficients of a polynomial
 
+    arg0: polynomial
+    """
+    for i in range(PARAMS_N):
+        r[i] = barrett_reduce(r[i])
+    return r
 
-def poly_montgomery_reduce():
-    pass
+def poly_montgomery_reduce(r):
+    '''
+    Applies Montgomery reduction to all coefficients of a polynomial
 
+    arg0: polynomial
+    '''
+    for i in range(PARAMS_N):
+        r[i] = montgomery_reduce(r[i])
+    return r
 
-def poly_base_mul():
-    pass
+def poly_base_mul(a, b):
+    '''
+    Multiplication of two polynomials in NTT domain
 
+    arg0: first polynomial
+    arg1: second polynomial
+    '''
+
+    for i in range(PARAMS_N // 4):
+        rx = base_multiplier(
+            a[4 * i + 2], a[4 * i + 3],
+            b[4 * i + 2], b[4 * i + 3],
+            NTT_ZETAS[64 + i]
+        )
+        ry = base_multiplier(
+            a[4*i+2], a[4*i+3],
+			b[4*i+2], b[4*i+3],
+            NTT_ZETAS[64+i] * -1
+        )
+        a[4*i+0] = rx[0]
+        a[4*i+1] = rx[1]
+        a[4*i+2] = ry[0]
+        a[4*i+3] = ry[1]
+    return a
 
 def poly_to_bytes(a):
     '''
@@ -51,8 +89,17 @@ def poly_to_bytes(a):
 
     arg0: polynomial
     '''
-    pass
-
+    t0 = 0
+    t1 = 0
+    r = [0 for _ in range(POLY_BYTES)]
+    a = poly_conditional_sub_q(a)
+    for i in range(PARAMS_N // 2):
+        t0 = uint16(a[2*i])
+        t1 = uint16(a[2*i+1])
+        r[3*i+0] = byte(t0 >> 0)
+        r[3*i+1] = byte(t0>>8) | byte(t1<<4)
+        r[3*i+2] = byte(t1 >> 4)
+    return r
 
 def poly_from_bytes(a):
     '''
@@ -60,17 +107,25 @@ def poly_from_bytes(a):
 
     arg0: byte array
     '''
-    pass
+    r = [0 for _ in range(PARAMS_N)]
+    for i in range(PARAMS_N // 2):
+        r[2*i] = int16(((uint16(a[3*i+0]) >> 0) | (uint16(a[3*i+1]) << 8)) & 0xFFF)
+        r[2*i+1] = int16(((uint16(a[3*i+1]) >> 4) | (uint16(a[3*i+2]) << 4)) & 0xFFF)
+    return r
 
-
-def poly_from_msg(a):
+def poly_from_msg(msg):
     '''
     Convert a 32-byte message to a polynomial.
 
     arg0: 32-byte message
     '''
-    pass
-
+    r = [0 for _ in range(PARAMS_N)]
+    mask = 0
+    for i in range(PARAMS_N // 8):
+        for j in range(8):
+            mask = -1 * int16((msg[i] >> j) & 1)
+            r[8*i+j] = mask & (PARAMS_Q // 2)
+    return r
 
 def poly_to_msg(a):
     '''
@@ -78,8 +133,13 @@ def poly_to_msg(a):
 
     arg0: polynomial
     '''
-    pass
-
+    msg = [0 for _ in range(32)]
+    a = poly_conditional_sub_q(a)
+    for i in range(PARAMS_N // 8):
+        for j in range(8):
+            t = (((uint16(a[8*i+j]) << 1) + uint16(PARAMS_Q/2)) / uint16(PARAMS_Q)) & 1
+            msg[i] |= byte(t << j)
+    return msg
 
 def poly_compress(a, k):
     '''
@@ -90,8 +150,8 @@ def poly_compress(a, k):
     '''
     rr = 0
     r = []
-    t = [0 for x in range(8)]
-    a = poly_csubq(a)
+    t = [0 for _ in range(8)]
+    a = poly_conditional_sub_q(a)
     if k == 2 or k == 3:
         r = [0 for _ in range(COMPRESSED_BYTES_512)]
         for i in range(PARAMS_N // 8):
@@ -123,8 +183,8 @@ def poly_decompress(a, k):
     arg0: byte array
     arg1: value of PARAM_K
     '''
-    r = [0 for x in range(384)]
-    t = [0 for x in range(8)]
+    r = [0 for _ in range(384)]
+    t = [0 for _ in range(8)]
     aa = 0
     if k == 2 or k == 3:
         for i in range(PARAMS_N // 2):
@@ -147,12 +207,24 @@ def poly_decompress(a, k):
     return r
 
 def get_noise_poly(seed, nonce, k):
-    pass
+    l = 0
+    if k == 2:
+        l = PARAMS_N * PARAMS_ETA_1 // 4
+    else:
+        l = PARAMS_N * PARAMS_ETA_2 // 4
+    buf = gen_prf_byte_array(seed, nonce, l)
+    return gen_cbd_pol(buf, k)
 
 
-def gen_prf_byte_array():
-    pass
-
-
-def poly_csubq(a):
-    pass
+def gen_prf_byte_array(l, key, nonce):
+    hash = [0 for _ in range(l)]
+    xof = SHAKE256.new()
+    new_key = [0 for _ in range(0, len(key) + 1)]
+    for i in range(0, len(key)):
+        new_key[i] = key[i]
+    new_key[len(key)] = nonce
+    new_key = [x & 0xff for x in new_key]
+    xof.update(bytearray(new_key))
+    hash = xof.read(l)
+    hash = [byte(x) for x in hash]
+    return hash
