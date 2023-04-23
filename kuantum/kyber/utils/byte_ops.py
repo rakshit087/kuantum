be0547c20139dc914a66bca56c3e78343256afc6
Rakshit Arora <rakshit087@gmail.com>
2023-04-23 09:59:56 +0530

    fix(byte_ops): fixed multiple issues with byte_ops

--- kuantum/kyber/utils/byte_ops.py
+++ kuantum/kyber/utils/byte_ops.py
@@ -2,19 +2,19 @@ from kuantum.kyber.utils.num_type import int16, int32, uint32
 from kuantum.kyber.utils.constants import PARAMS_N, PARAMS_ETA_1, PARAMS_ETA_2, POLY_BYTES
 
 
-def load24_bit(byte):
+def load24_bit(x):
     '''
     load 3 bytes into a 32-bit integer needed for Kyber 512
     arg0: input byte array
     return:  32-bit unsigned integer 
     '''
-    r = int32(byte[0])
-    r |= (int32(byte[1]) << 8)
-    r |= (int32(byte[2]) << 16)
+    r = x[0] & 0xff
+    r |= (x[1] & 0xff) << 8
+    r |= (x[2] & 0xff) << 16
     return r
 
 
-def load32_bit(byte):
+def load32_bit(x):
     '''
     load 4 bytes into a 32-bit integer needed for Kyber 768 
     and Kyber 1024
@@ -22,10 +22,10 @@ def load32_bit(byte):
     arg0: input byte array
     return:  32-bit unsigned integer
     '''
-    r = uint32(byte[0])
-    r = r | (uint32(byte[1]) << 8)
-    r = r | (uint32(byte[2]) << 16)
-    r = r | (uint32(byte[3]) << 24)
+    r = x[0] & 0xff # to mask negative values
+    r |= (x[1] & 0xff) << 8
+    r |= (x[2] & 0xff) << 16
+    r |= (x[3] & 0xff) << 24
     return r
 
 
@@ -47,16 +47,16 @@ def gen_cbd_pol(buff, eta):
             d += ((t >> 1) & 0x00249249)
             d += ((t >> 2) & 0x00249249)
             for j in range(0, 4):
-                a = int16((d >> (6 * j + 0)) & 0x7)
-                b = int16((d >> (6 * j + PARAMS_ETA_1)) & 0x7)
-                r[4 * i + j] = (a - b)
+                a = (d >> (6 * j + 0)) & 0x7
+                b = (d >> (6 * j + PARAMS_ETA_1)) & 0x7
+                r[4 * i + j] = a - b
     else:
         for i in range(0, PARAMS_N // 8):
             t = load32_bit(buff[4 * i:])
             d = t & 0x55555555
             d = d + ((t >> 1) & 0x55555555)
             for j in range(0, 8):
-                a = int16((d >> (4 * j + 0)) & 0x3)
-                b = int16((d >> (4 * j + PARAMS_ETA_2)) & 0x3)
+                a = ((d >> (4 * j + 0)) & 0x3)
+                b = ((d >> (4 * j + PARAMS_ETA_2)) & 0x3)
                 r[8 * i + j] = (a - b)
     return r
